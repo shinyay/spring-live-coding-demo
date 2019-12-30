20fe3bd097fcadf251bdbcd2fe712d68abad50a1
shinyay <shinya.com@gmail.com>
2019-12-30 15:15:31 +0900

    Add query function at Repository

--- spring/src/main/kotlin/io/pivotal/shinyay/repository/PersonRepository.kt
+++ spring/src/main/kotlin/io/pivotal/shinyay/repository/PersonRepository.kt
@@ -1,7 +1,14 @@
 package io.pivotal.shinyay.repository
 
+import io.pivotal.shinyay.entity.Person
 import org.springframework.stereotype.Repository
 
 @Repository
 class PersonRepository {
+
+    val persons = mutableListOf<Person>()
+
+    fun findAll() = persons
+
+    fun findById(id: Int) = persons.singleOrNull{ it.id == id }
 }
