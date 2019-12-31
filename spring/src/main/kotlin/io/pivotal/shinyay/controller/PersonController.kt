a359c8d4fa0bfc75e2fc887b719301fae6d130d5
shinyay <shinya.com@gmail.com>
2019-12-31 20:03:45 +0900

    Add query function at Controller

--- spring/src/main/kotlin/io/pivotal/shinyay/controller/PersonController.kt
+++ spring/src/main/kotlin/io/pivotal/shinyay/controller/PersonController.kt
@@ -1,9 +1,19 @@
 package io.pivotal.shinyay.controller
 
+import io.pivotal.shinyay.repository.PersonRepository
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/persons")
-class PersonController {
+class PersonController(val repository: PersonRepository) {
+
+    @GetMapping
+    fun findAll() = repository.findAll()
+
+    @GetMapping("/{id}")
+    fun findById(@PathVariable id: Int) = repository.findById(id)
+    
 }
