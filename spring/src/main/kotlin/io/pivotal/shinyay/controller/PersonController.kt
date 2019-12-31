01559b5a2f2e63c87eeea0c63f0c478be9c783fe
shinyay <shinya.com@gmail.com>
2019-12-31 20:09:59 +0900

    Add persistence function for controller

--- spring/src/main/kotlin/io/pivotal/shinyay/controller/PersonController.kt
+++ spring/src/main/kotlin/io/pivotal/shinyay/controller/PersonController.kt
@@ -1,10 +1,8 @@
 package io.pivotal.shinyay.controller
 
+import io.pivotal.shinyay.entity.Person
 import io.pivotal.shinyay.repository.PersonRepository
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/persons")
@@ -15,5 +13,7 @@ class PersonController(val repository: PersonRepository) {
 
     @GetMapping("/{id}")
     fun findById(@PathVariable id: Int) = repository.findById(id)
-    
+
+    @PostMapping
+    fun add(@RequestBody person: Person) = repository.save(person)
 }
