49e1c000cf6ec571a75daeb72c3922f91a392d21
shinyay <shinya.com@gmail.com>
2020-01-03 09:11:30 +0900

    Add Git and Build information injection

--- spring/src/main/kotlin/io/pivotal/shinyay/config/SwaggerConfig.kt
+++ spring/src/main/kotlin/io/pivotal/shinyay/config/SwaggerConfig.kt
@@ -1,9 +1,18 @@
 package io.pivotal.shinyay.config
 
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.info.BuildProperties
+import org.springframework.boot.info.GitProperties
 import org.springframework.context.annotation.Configuration
 import springfox.documentation.swagger2.annotations.EnableSwagger2
+import java.util.*
 
 @Configuration
 @EnableSwagger2
 class SwaggerConfig {
+
+    @Autowired
+    lateinit var build: Optional<BuildProperties>
+    @Autowired
+    lateinit var git: Optional<GitProperties>
 }
