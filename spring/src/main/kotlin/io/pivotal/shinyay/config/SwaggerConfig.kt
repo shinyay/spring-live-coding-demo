b19eddab2022c052bcb0379f1cf4cd37dc9634dc
shinyay <shinya.com@gmail.com>
2020-01-04 21:45:27 +0900

    Add Swagger UI Configuration funcrion

--- spring/src/main/kotlin/io/pivotal/shinyay/config/SwaggerConfig.kt
+++ spring/src/main/kotlin/io/pivotal/shinyay/config/SwaggerConfig.kt
@@ -1,13 +1,20 @@
 package io.pivotal.shinyay.config
 
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.info.BuildProperties
 import org.springframework.boot.info.GitProperties
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import springfox.documentation.swagger.web.UiConfiguration
+import springfox.documentation.swagger.web.UiConfigurationBuilder
 import springfox.documentation.swagger2.annotations.EnableSwagger2
 import java.util.*
 
 @Configuration
 @EnableSwagger2
 class SwaggerConfig(var build: Optional<BuildProperties>, var git: Optional<GitProperties>) {
+
+    @Bean
+    fun uiConfig(): UiConfiguration = UiConfigurationBuilder.builder()
+            .build()
+
 }
