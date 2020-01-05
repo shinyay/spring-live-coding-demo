0787f8e0c37cd3745f34c14dce8eba295f655cf5
shinyay <shinya.com@gmail.com>
2020-01-05 18:58:11 +0900

    Add Docket function

--- spring/src/main/kotlin/io/pivotal/shinyay/config/SwaggerConfig.kt
+++ spring/src/main/kotlin/io/pivotal/shinyay/config/SwaggerConfig.kt
@@ -5,7 +5,10 @@ import org.springframework.boot.info.GitProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import springfox.documentation.builders.ApiInfoBuilder
+import springfox.documentation.builders.RequestHandlerSelectors
 import springfox.documentation.service.ApiInfo
+import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spring.web.plugins.Docket
 import springfox.documentation.swagger.web.*
 import springfox.documentation.swagger2.annotations.EnableSwagger2
 import java.util.*
@@ -33,4 +36,22 @@ class SwaggerConfig(var build: Optional<BuildProperties>, var git: Optional<GitP
             .description("Person Management Service")
             .version(version)
             .build()
+
+    @Bean
+    fun docApi(): Docket {
+        var version = "0.0.1"
+        if(build.isPresent && git.isPresent) {
+            val buildInfo = build.get()
+            val gitInfo = git.get()
+            version = "${buildInfo.version}-${gitInfo.shortCommitId}-${gitInfo.branch}"
+        }
+        return Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo(version))
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths { it.equals("/persons") }
+                .build()
+                .useDefaultResponseMessages(false)
+                .forCodeGeneration(true)
+    }
 }
