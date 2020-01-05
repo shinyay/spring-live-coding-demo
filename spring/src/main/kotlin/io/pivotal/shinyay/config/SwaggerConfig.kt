758e205740bd0c8fcbc8d48cddc1ae0242752696
shinyay <shinya.com@gmail.com>
2020-01-05 18:48:08 +0900

    Add ApiInfo function

--- spring/src/main/kotlin/io/pivotal/shinyay/config/SwaggerConfig.kt
+++ spring/src/main/kotlin/io/pivotal/shinyay/config/SwaggerConfig.kt
@@ -4,6 +4,8 @@ import org.springframework.boot.info.BuildProperties
 import org.springframework.boot.info.GitProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import springfox.documentation.builders.ApiInfoBuilder
+import springfox.documentation.service.ApiInfo
 import springfox.documentation.swagger.web.*
 import springfox.documentation.swagger2.annotations.EnableSwagger2
 import java.util.*
@@ -24,5 +26,11 @@ class SwaggerConfig(var build: Optional<BuildProperties>, var git: Optional<GitP
             .tagsSorter(TagsSorter.ALPHA)
             .supportedSubmitMethods(UiConfiguration.Constants.DEFAULT_SUBMIT_METHODS)
             .validatorUrl(null)
+            .build()
+
+    private fun apiInfo(version: String): ApiInfo = ApiInfoBuilder()
+            .title("API for Person")
+            .description("Person Management Service")
+            .version(version)
             .build()
 }
