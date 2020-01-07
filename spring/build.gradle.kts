df71fe8dda7fb55eb49f614bbdbd33d182331132
shinyay <shinya.com@gmail.com>
2020-01-07 17:00:54 +0900

    Add a dependency for jetty and exclude tomcat

--- spring/build.gradle.kts
+++ spring/build.gradle.kts
@@ -18,7 +18,10 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
-	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-web") {
+		exclude(group = "org.springframework.boot", module = "spring-boot-starter-tomcat")
+	}
+	implementation("org.springframework.boot:spring-boot-starter-jetty")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("io.springfox:springfox-swagger2:2.9.2")
 	implementation("io.springfox:springfox-swagger-ui:2.9.2")
