5f8f9389f7b55b6bc71324e9c7eb796cb56c4dff
shinyay <shinya.com@gmail.com>
2020-01-30 08:02:04 +0900

    Add test data creation procedure

--- spring/src/main/kotlin/io/pivotal/shinyay/controller/PersonDBController.kt
+++ spring/src/main/kotlin/io/pivotal/shinyay/controller/PersonDBController.kt
@@ -1,11 +1,10 @@
 package io.pivotal.shinyay.controller
 
+import io.pivotal.shinyay.entity.Gender
 import io.pivotal.shinyay.entity.Person
 import io.pivotal.shinyay.repository.PersonRepositoryDBInterface
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
+import javax.annotation.PostConstruct
 
 @RestController
 @RequestMapping("/db")
@@ -14,9 +13,20 @@ class PersonDBController(val repository: PersonRepositoryDBInterface) {
     @GetMapping
     fun findAll(): MutableIterable<Person> = repository.findAll()
 
-    @GetMapping("/person/{name}")
+    @GetMapping("/person/name/{name}")
     fun findByName(@PathVariable name: String) = repository.findByName(name)
 
-    @GetMapping("/person/(id}")
+    @GetMapping("/person/{id}")
     fun findOne(@PathVariable id: Int) = repository.findById(id)
+
+    private fun create(person: Person) = repository.save(person)
+
+    @PostConstruct
+    fun demoData() {
+        create(Person(1, "Rei Ayanami", 14, Gender.FEMALE))
+        create(Person(2, "Shinji Ikari", 14, Gender.MALE))
+        create(Person(3, "Soryu Asuka Langley", 14, Gender.FEMALE))
+        create(Person(4, "Torji Suzuhara", 14, Gender.MALE))
+        create(Person(5, "Kaoru Nagisa", 14, Gender.MALE))
+    }
 }
