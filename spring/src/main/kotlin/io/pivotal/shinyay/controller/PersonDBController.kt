1026dc4f2bfd56cde97552e05289be6d79920dc9
shinyay <shinya.com@gmail.com>
2020-01-30 07:27:43 +0900

    Add repository function layer for person database

---
+++ spring/src/main/kotlin/io/pivotal/shinyay/controller/PersonDBController.kt
@@ -0,0 +1,22 @@
+package io.pivotal.shinyay.controller
+
+import io.pivotal.shinyay.entity.Person
+import io.pivotal.shinyay.repository.PersonRepositoryDBInterface
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/db")
+class PersonDBController(val repository: PersonRepositoryDBInterface) {
+
+    @GetMapping
+    fun findAll(): MutableIterable<Person> = repository.findAll()
+
+    @GetMapping("/person/{name}")
+    fun findByName(@PathVariable name: String) = repository.findByName(name)
+
+    @GetMapping("/person/(id}")
+    fun findById(@PathVariable id: Int) = repository.findById(id)
+}
