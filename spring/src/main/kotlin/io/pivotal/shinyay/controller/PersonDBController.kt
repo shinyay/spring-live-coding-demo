f9119ae0108fb665d3dacea74539610228c354b4
shinyay <shinya.com@gmail.com>
2020-01-31 09:19:49 +0900

    Modify test data entity

--- spring/src/main/kotlin/io/pivotal/shinyay/controller/PersonDBController.kt
+++ spring/src/main/kotlin/io/pivotal/shinyay/controller/PersonDBController.kt
@@ -13,8 +13,8 @@ class PersonDBController(val repository: PersonRepositoryDBInterface) {
     @GetMapping
     fun findAll(): MutableIterable<Person> = repository.findAll()
 
-    @GetMapping("/person/name/{name}")
-    fun findByName(@PathVariable name: String) = repository.findByName(name)
+    @GetMapping("/person/name/{firstname}")
+    fun findByName(@PathVariable firstname: String) = repository.findByFirstname(firstname)
 
     @GetMapping("/person/{id}")
     fun findOne(@PathVariable id: Int) = repository.findById(id)
@@ -23,10 +23,10 @@ class PersonDBController(val repository: PersonRepositoryDBInterface) {
 
     @PostConstruct
     fun demoData() {
-        create(Person(1, "Rei Ayanami", 14, Gender.FEMALE))
-        create(Person(2, "Shinji Ikari", 14, Gender.MALE))
-        create(Person(3, "Soryu Asuka Langley", 14, Gender.FEMALE))
-        create(Person(4, "Torji Suzuhara", 14, Gender.MALE))
-        create(Person(5, "Kaoru Nagisa", 14, Gender.MALE))
+        create(Person(1, "Rei", "Ayanami", 14, Gender.FEMALE))
+        create(Person(2, "Shinji", "Ikari", 14, Gender.MALE))
+        create(Person(3, "Asuka Langley", "Soryu", 14, Gender.FEMALE))
+        create(Person(4, "Torji", "Suzuhara", 14, Gender.MALE))
+        create(Person(5, "Kaoru", "Nagisa", 14, Gender.MALE))
     }
 }
