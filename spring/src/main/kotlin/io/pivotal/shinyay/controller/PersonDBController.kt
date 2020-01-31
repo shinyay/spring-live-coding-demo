46e94c88d0daccb8aca25e6a8946684a6d826932
shinyay <shinya.com@gmail.com>
2020-01-31 09:52:31 +0900

    Modify crud function name and annotation to align with on-memmory function

--- spring/src/main/kotlin/io/pivotal/shinyay/controller/PersonDBController.kt
+++ spring/src/main/kotlin/io/pivotal/shinyay/controller/PersonDBController.kt
@@ -3,30 +3,35 @@ package io.pivotal.shinyay.controller
 import io.pivotal.shinyay.entity.Gender
 import io.pivotal.shinyay.entity.Person
 import io.pivotal.shinyay.repository.PersonRepositoryDBInterface
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
 import javax.annotation.PostConstruct
 
 @RestController
-@RequestMapping("/db")
+@RequestMapping("/db/person")
 class PersonDBController(val repository: PersonRepositoryDBInterface) {
 
     @GetMapping
     fun findAll(): MutableIterable<Person> = repository.findAll()
 
-    @GetMapping("/person/name/{firstname}")
+    @GetMapping("/name/{firstname}")
     fun findByName(@PathVariable firstname: String) = repository.findByFirstname(firstname)
 
-    @GetMapping("/person/{id}")
+    @GetMapping("/{id}")
     fun findOne(@PathVariable id: Int) = repository.findById(id)
 
-    private fun create(person: Person) = repository.save(person)
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    fun add(@RequestBody person: Person) = repository.save(person)
+
+//    private fun create(person: Person) = repository.save(person)
 
     @PostConstruct
     fun demoData() {
-        create(Person(1, "Rei", "Ayanami", 14, Gender.FEMALE))
-        create(Person(2, "Shinji", "Ikari", 14, Gender.MALE))
-        create(Person(3, "Asuka Langley", "Soryu", 14, Gender.FEMALE))
-        create(Person(4, "Torji", "Suzuhara", 14, Gender.MALE))
-        create(Person(5, "Kaoru", "Nagisa", 14, Gender.MALE))
+        add(Person(1, "Rei", "Ayanami", 14, Gender.FEMALE))
+        add(Person(2, "Shinji", "Ikari", 14, Gender.MALE))
+        add(Person(3, "Asuka Langley", "Soryu", 14, Gender.FEMALE))
+        add(Person(4, "Torji", "Suzuhara", 14, Gender.MALE))
+        add(Person(5, "Kaoru", "Nagisa", 14, Gender.MALE))
     }
 }
