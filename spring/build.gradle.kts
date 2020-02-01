1a5e6647b7c4a41eb9e5ae45739d3d964b8799c2
shinyay <shinya.com@gmail.com>
2020-02-01 19:16:23 +0900

    Add Thymeleaf dependency

--- spring/build.gradle.kts
+++ spring/build.gradle.kts
@@ -26,6 +26,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-jetty")
 //	implementation("org.springframework.boot:spring-boot-starter-undertow")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
 	implementation("com.h2database:h2")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("io.springfox:springfox-swagger2:2.9.2")
