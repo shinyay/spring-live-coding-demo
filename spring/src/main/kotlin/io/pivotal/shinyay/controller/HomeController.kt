005aef61969cd7ce293e16a1c7b16fe8c27c9142
shinyay <shinya.com@gmail.com>
2020-02-08 20:30:28 +0900

    Add foward model to ui

--- spring/src/main/kotlin/io/pivotal/shinyay/controller/HomeController.kt
+++ spring/src/main/kotlin/io/pivotal/shinyay/controller/HomeController.kt
@@ -2,10 +2,18 @@ package io.pivotal.shinyay.controller
 
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.RequestMapping
 
 @Controller
 class HomeController {
 
     @Value("\${spring.application.name}")
     val appName: String = ""
+
+    @RequestMapping("/")
+    fun home(model: Model): String {
+        model.addAttribute("appName", appName)
+        return "home"
+    }
 }
