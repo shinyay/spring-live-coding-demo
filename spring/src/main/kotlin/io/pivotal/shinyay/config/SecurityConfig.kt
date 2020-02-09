9bc6d78801ae9e47746cb5b8c28aa8368409b5e4
shinyay <shinya.com@gmail.com>
2020-02-09 23:00:14 +0900

    Add configuration to permit all access

--- spring/src/main/kotlin/io/pivotal/shinyay/config/SecurityConfig.kt
+++ spring/src/main/kotlin/io/pivotal/shinyay/config/SecurityConfig.kt
@@ -1,10 +1,17 @@
 package io.pivotal.shinyay.config
 
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 
 @Configuration
 @EnableWebSecurity
 class SecurityConfig : WebSecurityConfigurerAdapter() {
+    override fun configure(http: HttpSecurity?) {
+        http?.authorizeRequests()
+                ?.anyRequest()
+                ?.permitAll()
+                ?.and()?.csrf()?.disable()
+    }
 }
