8b919aeafe0f9860f69b08fcf2523f11c61ba84e
shinyay <shinya.com@gmail.com>
2020-02-11 12:42:08 +0900

    Add test to confirm return body with hamcrest

--- spring/src/test/kotlin/io/pivotal/shinyay/SpringLiveCodingApplicationTests.kt
+++ spring/src/test/kotlin/io/pivotal/shinyay/SpringLiveCodingApplicationTests.kt
@@ -1,6 +1,7 @@
 package io.pivotal.shinyay
 
 import io.restassured.RestAssured.get
+import org.hamcrest.CoreMatchers.equalTo
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.web.server.LocalServerPort
@@ -16,5 +17,13 @@ class SpringLiveCodingApplicationTests(@LocalServerPort val port: Int) {
 		get(uri).then()
 				.assertThat()
 				.statusCode(HttpStatus.OK.value())
+	}
+
+	@Test
+	fun whenGetPersonById_thenOK() {
+		get("$uri/1").then()
+				.assertThat()
+				.statusCode(HttpStatus.OK.value())
+				.body("firstname", equalTo("Rei"))
 	}
 }
