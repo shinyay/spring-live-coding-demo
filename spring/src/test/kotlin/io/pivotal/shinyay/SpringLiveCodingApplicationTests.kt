ea5245fb3b5682ea73ab7b11a4b49b778f6dc0ee
shinyay <shinya.com@gmail.com>
2020-02-11 12:24:13 +0900

    Add test function with REST-assured

--- spring/src/test/kotlin/io/pivotal/shinyay/SpringLiveCodingApplicationTests.kt
+++ spring/src/test/kotlin/io/pivotal/shinyay/SpringLiveCodingApplicationTests.kt
@@ -1,15 +1,20 @@
 package io.pivotal.shinyay
 
+import io.restassured.RestAssured.get
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.web.server.LocalServerPort
+import org.springframework.http.HttpStatus
 
-@SpringBootTest
-class SpringLiveCodingApplicationTests {
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class SpringLiveCodingApplicationTests(@LocalServerPort val port: Int) {
 
-	val ENDPOINT = "http://localhost:8080//db/person"
+	val uri = "http://localhost:${port}/db/person"
 
 	@Test
-	fun contextLoads() {
+	fun whenGetAllPersons_thenOK() {
+		get(uri).then()
+				.assertThat()
+				.statusCode(HttpStatus.OK.value())
 	}
-
 }
