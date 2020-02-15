1a7b194e131e7acdc1e4f47d15325eb2c3714de6
shinyay <shinya.com@gmail.com>
2020-02-15 23:00:17 +0900

    Add POST test with using Google Truth

--- spring/src/test/kotlin/io/pivotal/shinyay/SpringLiveCodingApplicationTests.kt
+++ spring/src/test/kotlin/io/pivotal/shinyay/SpringLiveCodingApplicationTests.kt
@@ -1,11 +1,19 @@
 package io.pivotal.shinyay
 
+import io.pivotal.shinyay.entity.Gender
+import io.pivotal.shinyay.entity.Person
+import io.restassured.RestAssured
 import io.restassured.RestAssured.get
+import io.restassured.response.Response
+import org.apache.commons.lang3.RandomStringUtils
+import org.apache.commons.lang3.RandomUtils
 import org.hamcrest.CoreMatchers.equalTo
 import org.junit.jupiter.api.Test
+import com.google.common.truth.Truth.assertThat
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.web.server.LocalServerPort
 import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SpringLiveCodingApplicationTests(@LocalServerPort val port: Int) {
@@ -26,4 +34,22 @@ class SpringLiveCodingApplicationTests(@LocalServerPort val port: Int) {
 				.statusCode(HttpStatus.OK.value())
 				.body("firstname", equalTo("Rei"))
 	}
+
+	@Test
+	fun whenCreatePerson_thenCreated() {
+		val person = createPerson()
+		println(person)
+		val response: Response = RestAssured.given()
+				.contentType(MediaType.APPLICATION_JSON_VALUE)
+				.body(person)
+				.post(uri)
+		assertThat(response.statusCode).isEqualTo(HttpStatus.CREATED.value())
+	}
+
+	private fun createPerson(): Person = Person(
+			RandomUtils.nextInt(10, 100),
+			RandomStringUtils.randomAlphabetic(5),
+			RandomStringUtils.randomAlphabetic(5),
+			RandomUtils.nextInt(10, 100),
+			Gender.FEMALE)
 }
